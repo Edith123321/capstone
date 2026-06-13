537eaa55f79d28f4a3a6559c6779f85c3cf1b310
edith123321 <edithgithinji2020@gmail.com>
2026-06-13 09:39:55 +0200

    Add initial project structure and configuration files

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p backend/api/v1/auth
+mkdir -p backend/api/v1/screening
+mkdir -p backend/core
+mkdir -p backend/db/migrations
+
+
+
+echo "Capstone project structure created successfully!"
